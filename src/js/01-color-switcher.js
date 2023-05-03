36b04bfe59dc5ecacc4b95d18bd19c4cdbc2c582
Tensys57 <tensys57@gmail.com>
2023-05-04 00:17:08 +0300

    Task 9-1 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const refs = {
+  body: document.body,
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+let timerId = 0;
+function startHandler() {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+  // refs.startBtn.setAttribute('disabled', 'true');
+  refs.stopBtn.style.backgroundColor = 'lightyellow';
+  refs.startBtn.style.backgroundColor = 'gray';
+  // refs.startBtn.removeEventListener('click', startHandler);
+  refs.stopBtn.addEventListener('click', stopHandler);
+}
+function stopHandler() {
+  clearInterval(timerId);
+  // refs.startBtn.setAttribute('disabled', 'false');
+  refs.startBtn.style.backgroundColor = 'lightyellow';
+  refs.stopBtn.style.backgroundColor = 'gray';
+  // refs.stopBtn.removeEventListener('click', stopHandler);
+  // refs.startBtn.addEventListener('click', startHandler);
+}
 
+refs.startBtn.addEventListener('click', startHandler);
