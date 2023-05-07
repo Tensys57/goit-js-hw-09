05875f6ad51516bb1f05b27cefdf803edd47daee
Tensys57 <tensys57@gmail.com>
2023-05-07 20:35:19 +0300

    Task 9-3 fulfiled

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+const submitFormRef = document.querySelector('.form');
+
+function submitHandler(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.currentTarget.elements;
+  const formData = {
+    delay: delay.value,
+    step: step.value,
+    amount: amount.value,
+  };
+  let currentDelay = Number(formData.delay);
+  for (
+    let idPromise = 1;
+    idPromise <= Number(formData.amount);
+    idPromise += 1
+  ) {
+    createPromise(idPromise, currentDelay)
+      .then(object => {
+        const { position, delay } = object;
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(object => {
+        const { position, delay } = object;
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    currentDelay += Number(formData.step);
   }
 }
+
+function createPromise(position, delay) {
+  return (nextPromise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  }));
+}
+
+submitFormRef.addEventListener('submit', submitHandler);
