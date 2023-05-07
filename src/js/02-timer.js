86bb7d879253c0e22c6c1878586fc3532edcfc15
Tensys57 <tensys57@gmail.com>
2023-05-07 21:07:27 +0300

    Task 9-2 styled

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,15 +1,7 @@
-// Напиши скрипт таймера, який здійснює зворотний відлік до певної дати.
-// Вибір дати
-// Метод onClose() з об'єкта параметрів викликається щоразу під час закриття елемента інтерфейсу, який створює flatpickr. Саме у ньому варто обробляти дату, обрану користувачем. Параметр selectedDates - це масив обраних дат, тому ми беремо перший елемент.
-
-// Якщо користувач вибрав дату в минулому, покажи window.alert() з текстом "Please choose a date in the future".
-// Якщо користувач вибрав валідну дату (в майбутньому), кнопка «Start» стає активною.
-// Кнопка «Start» повинна бути неактивною доти, доки користувач не вибрав дату в майбутньому.
-// Натисканням на кнопку «Start» починається відлік часу до обраної дати з моменту натискання.
-
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
+
 const refs = {
   pickerInput: document.querySelector('#datetime-picker'),
   startBtn: document.querySelector('button[data-start]'),
@@ -18,6 +10,7 @@ const refs = {
   minutesValue: document.querySelector('[data-minutes]'),
   secondsValue: document.querySelector('[data-seconds]'),
 };
+
 refs.startBtn.setAttribute('disabled', 'true');
 const options = {
   enableTime: true,
@@ -51,19 +44,14 @@ const options = {
 };
 flatpickr(refs.pickerInput, options);
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
   return { days, hours, minutes, seconds };
 }
